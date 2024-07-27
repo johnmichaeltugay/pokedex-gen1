803f19152a8c4b5cd20bd3b37696fa02f4792c71
John Michael Tugay <johnmichael.tugay@gmail.com>
2024-07-27 20:30:48 +0800

    Restructured Build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.scss'
 import axios from 'axios'
 
 function App() {
@@ -9,7 +7,7 @@ function App() {
 
   axios.get('https://pokeapi.co/api/v2/pokemon/', {
     params: {
-      limit: "all",
+      limit: "??",
     }
   })
     .then((response) => {
@@ -25,24 +23,17 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      <div className="bg-green-500 w-screen h-screen">
-        
+       */}
+      <div className="w-full h-full">
+        Landing Area
+        <button> hello </button>
+        <a>Link sample</a>
       </div>
     </>
   )
