8f939d098af2a375b026f7d5057bf27b695136ef
John Michael Tugay <johnmichael.tugay@gmail.com>
2024-07-29 17:40:40 +0800

    Incremental Fetching 2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/App.scss'
 import axios from 'axios'
 
@@ -11,21 +11,45 @@ function App() {
   const loadData = async () => {
     await axios.get('https://pokeapi.co/api/v2/pokemon/', {
       params: {
-        limit: loadSize,
+        limit: load,
         offset: offset,
       }
     })
       .then((response) => {
+        console.log("loadData");
         setLoad((prevLoad) => prevLoad += loadSize)
         setOffset((prevOffset) => prevOffset += loadSize);
         console.log(response);
-        console.log(typeof response.data.results, response.data.results);
+        response.data.results.forEach(element => {
+          console.log(element);
+        });
     })
       .catch((error) => {
         console.log("Unable to fetch data.");
         console.log(error);
       })
   }
+
+  useEffect(() => {
+    console.log("useEffect");
+    axios.get('https://pokeapi.co/api/v2/pokemon/', {
+      params: {
+        limit: load,
+        offset: offset,
+      }
+    })
+      .then((response) => {
+        response.data.results.forEach(element => {
+          console.log(element);
+          setLoad((prevLoad) => prevLoad++);
+          setOffset((prevOffset) => prevOffset++);
+        });
+    })
+      .catch((error) => {
+        console.log("Unable to fetch data.");
+        console.log(error);
+      })
+  }, [])
 
   return (
     <>
@@ -41,7 +65,7 @@ function App() {
       </div>
        */}
       <div className="w-full h-full flex flex-row justify-center items-baseline gap-x-4">
-        <div className='bg-red-500 max-h-content'>{ load }</div>
+        <div className='bg-red-500 max-h-content'>{ pokeData }</div>
         <button onClick={ () => loadData() }> hello </button>
         <a>Link sample</a>
 
