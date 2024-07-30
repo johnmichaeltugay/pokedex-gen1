a0f892898a0e451dc7fd0ecce56bc2c53ce2c9f1
John Michael Tugay <johnmichael.tugay@gmail.com>
2024-07-30 23:25:53 +0800

    Pokemon Tiles: UI only

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import '../styles/App.scss'
 import axios from 'axios'
 import useLocalStorage from '../hooks/useLocalStorage';
+import PokemonRoster from './PokemonRoster';
+import { PokemonRosterContext } from '../contexts/PokemonRosterContext';
 
 function App() {
   const loadSize = 15;
@@ -11,7 +13,144 @@ function App() {
   const [load, setLoad] = useLocalStorage(loadSizeCacheName, loadSize);
   const [offset, setOffset] = useLocalStorage(offsetSizeCacheName, 0);
   // const [pokeData, setPokeData] = useLocalStorage(pokeDataCacheName, []);
-  
+
+  const pokeData = [
+    {
+      'id': 1,
+      'name': 'bulbasaur',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg',
+      'type': ['grass', 'poison'],
+      'weight': 69,
+      'height': 7,
+      'order': 1,
+    },
+    {
+      'id': 2,
+      'name': 'ivysaur',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg',
+      'type': ['grass', 'poison'],
+      'weight': 130,
+      'height': 10,
+      'order': 2,
+    },
+    {
+      'id': 3,
+      'name': 'venusaur',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg',
+      'type': ['grass', 'poison'],
+      'weight': 1000,
+      'height': 20,
+      'order': 3,
+    },
+    {
+      'id': 4,
+      'name': 'charmander',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg',
+      'type': ['fire'],
+      'weight': 85,
+      'height': 6,
+      'order': 5,
+    },
+    {
+      'id': 5,
+      'name': 'charmeleon',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg',
+      'type': ['fire'],
+      'weight': 190,
+      'height': 11,
+      'order': 6,
+    },
+    {
+      'id': 6,
+      'name': 'charizard',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg',
+      'type': ['fire', 'flying'],
+      'weight': 905,
+      'height': 17,
+      'order': 7,
+    },
+    {
+      'id': 7,
+      'name': 'squirtle',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg',
+      'type': ['water'],
+      'weight': 90,
+      'height': 5,
+      'order': 10,
+    },
+    {
+      'id': 8,
+      'name': 'wartortle',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/8.svg',
+      'type': ['water'],
+      'weight': 225,
+      'height': 10,
+      'order': 11,
+    },
+    {
+      'id': 9,
+      'name': 'blastoise',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg',
+      'type': ['water'],
+      'weight': 855,
+      'height': 16,
+      'order': 12,
+    },
+    {
+      'id': 10,
+      'name': 'caterpie',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg',
+      'type': ['bug'],
+      'weight': 29,
+      'height': 3,
+      'order': 14,
+    },
+    {
+      'id': 11,
+      'name': 'metapod',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg',
+      'type': ['bug'],
+      'weight': 99,
+      'height': 7,
+      'order': 15,
+    },
+    {
+      'id': 12,
+      'name': 'butterfree',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/12.svg',
+      'type': ['bug', 'flying'],
+      'weight': 320,
+      'height': 11,
+      'order': 16,
+    },
+    {
+      'id': 13,
+      'name': 'weedle',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/13.svg',
+      'type': ['bug', 'poison'],
+      'weight': 32,
+      'height': 3,
+      'order': 17,
+    },
+    {
+      'id': 14,
+      'name': 'kakuna',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/14.svg',
+      'type': ['bug', 'poison'],
+      'weight': 100,
+      'height': 6,
+      'order': 18,
+    },
+    {
+      'id': 15,
+      'name': 'beedrill',
+      'sprites_other_dreamWorld_frontDefault': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/15.svg',
+      'type': ['bug', 'poison'],
+      'weight': 295,
+      'height': 10,
+      'order': 19,
+    },
+  ]
 
   const setNewNumberLimit = (dataFunction: number, localStorageName:string) => {
     localStorage.setItem(localStorageName, JSON.stringify(dataFunction));
@@ -25,14 +164,12 @@ function App() {
       }
     })
       .then((response) => {
+        console.log('loadData', response.data.results);
         setLoad((prevLoad: number) => prevLoad += loadSize);
         setOffset((prevOffset: number) => prevOffset += loadSize);
         setNewNumberLimit(load, loadSizeCacheName);
         setNewNumberLimit(offset, offsetSizeCacheName);
-        console.log('loadData', response);
-        response.data.results.forEach((element:object) => {
-          console.log(typeof element);
-        });
+        
     })
       .catch((error) => {
         console.log("Unable to fetch data.");
@@ -41,7 +178,6 @@ function App() {
   }
 
   useEffect(() => {
-    console.log("useEffect");
     localStorage.setItem('loadSize', JSON.stringify(load));
     axios.get('https://pokeapi.co/api/v2/pokemon/', {
       params: {
@@ -50,15 +186,11 @@ function App() {
       }
     })
       .then((response) => {
-        console.log('useEffect', response);
-        // response.data.results.forEach((element:object) => {
-          // setPokeData((prevPokeData:Array<object>) => [...prevPokeData, element]);
-        // });
+        // console.log('useEffect', response.data.results);
         setLoad((prevLoad: number) => prevLoad += loadSize);
         setOffset((prevOffset: number) => prevOffset += loadSize);
         setNewNumberLimit(load, loadSizeCacheName);
         setNewNumberLimit(offset, offsetSizeCacheName);
-        console.log("pokeData", pokeData);
     })
       .catch((error) => {
         console.log("Unable to fetch data.");
@@ -66,8 +198,10 @@ function App() {
       })
   }, [])
 
+
+
   return (
-    <>
+    <div className="w-screen max-w-screen-md">
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -79,13 +213,19 @@ function App() {
         </button>
       </div>
        */}
-      <div className="w-full h-full flex flex-row justify-center items-baseline gap-x-4">
+      <div className="bg-red-400 h-[8vh] sticky top-0 z-[100]">
+        <h1 className='text-4xl'>Pok&#233;dex</h1>
+      </div>
+      <PokemonRosterContext.Provider value={pokeData}>
+        <PokemonRoster />
+      </PokemonRosterContext.Provider>
+      <div className="w-full flex flex-row justify-center items-baseline gap-x-4 sticky bottom-0 z-[100]">
         <div className='bg-red-500 max-h-content'>{ load }</div>
         <button onClick={ () => loadData() }> hello </button>
         <a>Link sample</a>
 
       </div>
-    </>
+    </div>
   )
 }
 
